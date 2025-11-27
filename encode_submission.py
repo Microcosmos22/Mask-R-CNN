19392cd2e33fd7a04e29f43d6bd050d90966f839
Andres <andres.husterzapke@stud.uni-goettingen.de>
2025-11-27 10:38:44 +0100

    Work on showing the final mask+image results

--- encode_submission.py
+++ encode_submission.py
@@ -81,26 +81,85 @@ def rle_decode(mask_rle: str, shape: tuple[int, int]) -> npt.NDArray:
     except ValueError as e:
         raise ParticipantVisibleError(str(e)) from e
 
-model = create_light_mask_rcnn()
+model = create_light_mask_rcnn(feat_ex = 1)
 state = torch.load("best_model.pth", map_location="cpu")
 model.load_state_dict(state)
 model.eval()
 
-files = [base_path+number for number in os.listdir(base_path)]
+files = [base_path+"supplemental_images/"+number for number in os.listdir(base_path+"supplemental_images/")]
+
+print(files)
 
 
-for image, target in test_loader:
+for idx, (image, target) in enumerate(test_loader):
     # a loader with collate_fn returns batches of lists
     image = image[0]           # take first item from batch
     target = target[0]
+    raw_image, raw_mask = test_dataset.get_raw_img_mask(idx)
+
+    print(raw_image.shape)
 
     with torch.no_grad():
         outputs = model([image])   # must be list
         pred = outputs[0]
 
+
+        def full_mask_from_instance_masks(output, image_shape):
+            """
+            output: dict from MaskRCNN
+            image_shape: (H, W, C) of original image
+            """
+            H, W, _ = image_shape
+            full_mask = torch.zeros((H, W), dtype=torch.uint8)
+
+            for box, mask in zip(output['boxes'], output['masks']):
+                # clamp box coordinates to image bounds
+                x1, y1, x2, y2 = box.int()
+
+                if x2 <= x1 or y2 <= y1 or x1<0 or y1<0 or x2>W or y2>H:
+                    continue  # skip degenerate boxes
+
+                # resize mask to box size
+                mask_resized = F.interpolate(
+                    mask[None], size=(y2 - y1, x2 - x1),
+                    mode='bilinear', align_corners=False
+                )[0, 0]
+
+                print(f"\n shapes mask, mask[None] and mask_resized:")
+                print(mask.shape, mask.unsqueeze(0).shape, mask_resized.shape)
+
+                mask_bin = (mask_resized > 0.5).byte()
+                """ actual pasting of the submask"""
+                full_mask[y1:y2, x1:x2] = mask_bin
+
+            return full_mask
+
+        pred_mask = full_mask_from_instance_masks(pred, image.shape)  # shape = network input (H_net, W_net)
+
+        print(f"pred_mask shape {pred_mask.shape}")
+        # pred_mask is (H_net, W_net)
+        H_orig, W_orig, _ = raw_image.shape
+
+        # Ensure float for interpolation
+        pred_mask_resized = F.interpolate(
+            pred_mask.unsqueeze(0).unsqueeze(0).float(),  # (1,1,H_net,W_net)
+            size=(H_orig, W_orig),                        # (height, width) in original image
+            mode='nearest'
+        )[0, 0].byte()  # back to (H_orig, W_orig)
+
+        if idx >= len(files):
+            break
+        case_id = files[idx]
+
+
+        """ Convert to numpy and encode """
         submission = {
-        "case_id": files,
-        "submission": rle_encode(pred_mask)}
+            "case_id": files[idx],
+            "submission": rle_encode([pred_mask_resized.numpy()])
+        }
+        plt.imshow(raw_image)
+        plt.imshow(pred_mask_resized, alpha=0.5)
+        plt.show()
 
         print(pred.keys())
         print(pred["masks"].shape)
