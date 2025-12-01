ef9df6b675b4251e505b84931d6929074c4ccb0c
Andres <andres.husterzapke@stud.uni-goettingen.de>
2025-12-01 10:49:20 +0100

    Moved mask full combination to scoring

--- encode_submission.py
+++ encode_submission.py
@@ -8,8 +8,9 @@ import pandas as pd
 import scipy.optimize
 
 from edarnn import *
-from scoring import *
 from dataset import *
+from scoring import *
+
 
 
 class ParticipantVisibleError(Exception):
@@ -82,90 +83,60 @@ def rle_decode(mask_rle: str, shape: tuple[int, int]) -> npt.NDArray:
         raise ParticipantVisibleError(str(e)) from e
 
 
-def full_mask_from_instance_masks(output, image_shape):
-    """
-    output: dict from MaskRCNN
-    image_shape: (H, W, C) of original image
-    """
-    C, H, W = image_shape
-    full_mask = torch.zeros((H, W), dtype=torch.uint8)
-
-    if output['boxes'].shape[0] == 0:
-        print("Did not detect any submasks")
-        return torch.zeros((image_shape[0], image_shape[1]))
-    print(f"Detected {len(output['boxes'] == 0)} submasks")
-
-    for box, mask in zip(output['boxes'], output['masks']):
-        # clamp box coordinates to image bounds
-        x1, y1, x2, y2 = box.int()
-
-        if x2 <= x1 or y2 <= y1 or x1<0 or y1<0 or x2>W or y2>H:
-            continue  # skip degenerate boxes
-
-        """ Resize submask from 256 to the box size"""
-        mask_resized = F.interpolate(
-            mask[None], size=(y2 - y1, x2 - x1),
-            mode='bilinear', align_corners=False
-        )[0, 0]
-
-
-        mask_bin = (mask_resized > 0.5).byte()
-        """ actual pasting of the submask"""
-        full_mask[y1:y2, x1:x2] = mask_bin
-
-    return full_mask
-
-
-model = create_light_mask_rcnn(feat_ex = 1)
-state = torch.load("best_model_kaggle1.pth", map_location="cpu")
+model = create_light_mask_rcnn(feat_ex = 0)
+state = torch.load("best_overfit_machine.pth", map_location="cpu")
 model.load_state_dict(state)
 model.eval()
 
-files = [base_path+"supplemental_images/"+number for number in os.listdir(base_path+"supplemental_images/")]
+base_path = "../recodai-luc-scientific-image-forgery-detection/"
+test_dataset = ForgeryDataset(
+    paths['train_authentic'],
+    paths['train_forged'],
+    paths['train_masks'],
+)
+""" transform = train_transform """
 
-for idx, (image, target) in enumerate(test_loader):
-    """ skip authentic images """
-    """if (len(target[0]['boxes']) == 0):
-        continue"""
-    # a loader with collate_fn returns batches of lists
-    image = image[0]           # take first item from batch
-    target = target[0]
-    raw_image, raw_mask = test_dataset.get_raw_img_mask(idx)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
 
+if __name__ == "__main__":
 
-    with torch.no_grad():
-        outputs = model([image])   # must be list
-        pred = outputs[0]
-
-        full_pred_mask = full_mask_from_instance_masks(pred, image.shape)  # shape = network input (H_net, W_net)
-
-        if torch.sum(full_pred_mask) > 10:
-
-            print(f"pred_mask shape {full_pred_mask.shape}")
-            # pred_mask is (H_net, W_net)
-            H_orig, W_orig, _ = raw_image.shape
+    print("test")
+    for idx, (image, target) in enumerate(test_loader):
+        """ skip authentic images """
+        """if (len(target[0]['boxes']) == 0):
+            continue"""
+        # a loader with collate_fn returns batches of lists
+        image = image[0]           # take first item from batch
+        target = target[0]
+        raw_image, raw_mask = test_dataset.get_raw_img_mask(idx)
 
 
 
-            # Ensure float for interpolation
-            full_pred_mask_resized = F.interpolate(
-                full_pred_mask.unsqueeze(0).unsqueeze(0).float(),  # (1,1,H_net,W_net)
-                size=(H_orig, W_orig),                        # (height, width) in original image
-                mode='nearest'
-            )[0, 0].byte()  # back to (H_orig, W_orig)
+
+        with torch.no_grad():
+            outputs = model([image])   # must be list
 
 
-            plt.imshow(raw_image)
-            plt.imshow(full_pred_mask_resized, alpha=0.5)
-            plt.show()
+            full_pred_mask = full_mask_from_instance_masks(outputs[0], image.shape)  # shape = network input (H_net, W_net)
+
+            if torch.sum(full_pred_mask) > 10:
+
+                print(f"pred_mask shape {full_pred_mask.shape}")
+                # pred_mask is (H_net, W_net)
+                H_orig, W_orig, _ = raw_image.shape
 
 
-            """ Convert to numpy and encode """
-            submission = {
-                "case_id": files[int(idx*4)],
-                "submission": rle_encode([full_pred_mask_resized.numpy()])
-            }
+                plt.imshow(raw_image)
+                plt.imshow(full_pred_mask, alpha=0.5)
+                plt.show()
 
 
-            #rle = rle_encode(full_pred_mask_resized.numpy())
-            #print(f"rle encoded mask: {rle}")
+                """ Convert to numpy and encode """
+                """submission = {
+                    "case_id": files[int(idx*4)],
+                    "submission": rle_encode([full_pred_mask_resized.numpy()])
+                }"""
+
+
+                #rle = rle_encode(full_pred_mask_resized.numpy())
+                #print(f"rle encoded mask: {rle}")
