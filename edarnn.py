9a803687abf64e99e8b8825607a4c09cf9bc6d70
Andres <andres.husterzapke@stud.uni-goettingen.de>
2025-12-10 12:46:05 +0100

    again freeze mask head and predictor

--- edarnn.py
+++ edarnn.py
@@ -160,14 +160,14 @@ rpn_pre_train = 1000, rpn_pre_test = 1000, rpn_post_train=200, rpn_post_test=200
         box_detections_per_img=100
     )
 
-    """
+
     for p in model.roi_heads.mask_head.parameters():
         p.requires_grad = False
 
     for p in model.roi_heads.mask_predictor.parameters():
         p.requires_grad = False
     model.roi_heads.mask_on = False
-    """
+
 
     for p in model.backbone.parameters():
         p.requires_grad = False
